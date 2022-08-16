de06e0d51797920abd1adc4149790e6a913c1628
Beatriz Ramos <beatrizrmcarvalho@gmail.com>
2022-08-16 16:28:37 -0300

    commit inicial

---
+++ src/main/java/com/serratec/backend/PsNekiApplication.java
@@ -0,0 +1,13 @@
+package com.serratec.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsNekiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsNekiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/serratec/backend/PsNekiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serratec.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsNekiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
