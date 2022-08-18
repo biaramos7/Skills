6ce59af4c7a688d7b0614a9ec17b6f909d5331cd
Beatriz Ramos <beatrizrmcarvalho@gmail.com>
2022-08-18 09:13:35 -0300

    finaliza autenticação

--- src/main/java/com/serratec/backend/security/JWTAuthenticationFilter.java
+++ src/main/java/com/serratec/backend/security/JWTAuthenticationFilter.java
@@ -1,7 +1,6 @@
 package com.serratec.backend.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.SneakyThrows;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
